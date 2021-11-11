a3324240fff7ff752c7310ab250568d34edb5b20
Haya Joudeh <hayajoudeh20@gmail.com>
2021-11-11 12:32:51 +0200

    Add files

---
+++ POS/Controllers/LanguageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POS.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult SetLanguage(string lang)
+        {
+            return View();
+        }
+    }
+}
---
+++ POS/Controllers/POSController.cs
@@ -0,0 +1,21 @@
+﻿using POS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POS.Controllers
+{
+    public class POSController : Controller
+    {
+        // GET: POS
+        public ActionResult Index()
+        {
+            Main main = new Main();
+
+            main.store = new Store();
+            return View(main);
+        }
+    }
+}
