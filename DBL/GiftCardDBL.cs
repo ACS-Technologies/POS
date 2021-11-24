774fa8c30b385dc677b808c043638e5e2abcc367
Malak Majed <malakalmallak33@gmail.com>
2021-11-24 17:01:34 +0200

    add gift card poco, dbl,dal ,controller

---
+++ DBL/GiftCardDBL.cs
@@ -0,0 +1,210 @@
+﻿using DAL;
+using POCO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace DBL
+{
+    public class GiftCardDBL
+    {
+        GiftCardDAL oGiftCardsDAL = new GiftCardDAL();
+
+        /// <summary>
+        /// M_GiftCards_GetAll
+        /// </summary>
+        /// <returns></returns>
+        public List<GiftCard> M_GiftCards_GetAll()
+        {
+            DataSet ds = new DataSet();
+            GiftCard oGiftCards;
+            List<GiftCard> oLGiftCards = new List<GiftCard>();
+            ds = oGiftCardsDAL.M_GiftCards_GetAll();
+            if (ds.Tables.Count > 0)
+            {
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    oGiftCards = new GiftCard();
+                   
+                    oGiftCards.Id = int.Parse(ds.Tables[0].Rows[i]["Id"].ToString());
+                    oGiftCards.Card_no = ds.Tables[0].Rows[i]["Card_no"].ToString();
+                    oGiftCards.Balance = decimal.Parse(ds.Tables[0].Rows[i]["Balance"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[i]["Customer_id"].ToString()))
+                        oGiftCards.Customer_id = int.Parse(ds.Tables[0].Rows[i]["Customer_id"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[i]["Expiry"].ToString()))
+                        oGiftCards.Expiry = DateTime.Parse(ds.Tables[0].Rows[i]["Expiry"].ToString());
+                   
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[i]["Created_by"].ToString()))
+                        oGiftCards.Created_by = int.Parse(ds.Tables[0].Rows[i]["Created_by"].ToString());
+                    
+                    oGiftCards.Date = DateTime.Parse(ds.Tables[0].Rows[i]["Date"].ToString());
+                    oGiftCards.Store_id = int.Parse(ds.Tables[0].Rows[i]["Store_id"].ToString());
+                    oGiftCards.Value = decimal.Parse(ds.Tables[0].Rows[i]["Value"].ToString());
+
+                    oLGiftCards.Add(oGiftCards);
+                }
+            }
+            return oLGiftCards;
+        }
+
+        /// <summary>
+        /// M_GiftCards_GetById
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public GiftCard M_GiftCards_GetById(int Id)
+        {
+            DataSet ds = new DataSet();
+            GiftCard oGiftCards = new GiftCard();
+
+            ds = oGiftCardsDAL.M_GiftCards_GetById(Id);
+
+            if (ds.Tables.Count > 0)
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    oGiftCards.Id = int.Parse(ds.Tables[0].Rows[0]["Id"].ToString());
+                    oGiftCards.Card_no = ds.Tables[0].Rows[0]["Card_no"].ToString();
+                    oGiftCards.Balance = decimal.Parse(ds.Tables[0].Rows[0]["Balance"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Customer_id"].ToString()))
+                        oGiftCards.Customer_id = int.Parse(ds.Tables[0].Rows[0]["Customer_id"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Expiry"].ToString()))
+                        oGiftCards.Expiry = DateTime.Parse(ds.Tables[0].Rows[0]["Expiry"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Created_by"].ToString()))
+                        oGiftCards.Created_by = int.Parse(ds.Tables[0].Rows[0]["Created_by"].ToString());
+
+                    oGiftCards.Date = DateTime.Parse(ds.Tables[0].Rows[0]["Date"].ToString());
+                    oGiftCards.Store_id = int.Parse(ds.Tables[0].Rows[0]["Store_id"].ToString());
+                    oGiftCards.Value = decimal.Parse(ds.Tables[0].Rows[0]["Value"].ToString());
+                }
+
+            }
+            return oGiftCards;
+        }
+
+        /// <summary>
+        /// M_GiftCards_Delete
+        /// </summary>
+        /// <param name="Id"></param>
+        public void M_GiftCards_Delete(int Id)
+        {
+            DataSet ds = new DataSet();
+            GiftCard oGiftCards = new GiftCard();
+            ds = oGiftCardsDAL.M_GiftCards_Delete(Id);
+        }
+
+        public GiftCard M_Store_Insert(GiftCard PoGiftCards)
+        {
+            DataSet ds = new DataSet();
+            GiftCard oGiftCards = new GiftCard();
+            ds = oGiftCardsDAL.M_GiftCards_Insert(PoGiftCards.Card_no.Trim(), PoGiftCards.Value, PoGiftCards.Customer_id, PoGiftCards.Balance, PoGiftCards.Expiry
+                , PoGiftCards.Created_by, PoGiftCards.Store_id);
+
+            if (ds.Tables.Count > 0)
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    oGiftCards.Id = int.Parse(ds.Tables[0].Rows[0]["Id"].ToString());
+                    oGiftCards.Card_no = ds.Tables[0].Rows[0]["Card_no"].ToString();
+                    oGiftCards.Balance = decimal.Parse(ds.Tables[0].Rows[0]["Balance"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Customer_id"].ToString()))
+                        oGiftCards.Customer_id = int.Parse(ds.Tables[0].Rows[0]["Customer_id"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Expiry"].ToString()))
+                        oGiftCards.Expiry = DateTime.Parse(ds.Tables[0].Rows[0]["Expiry"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Created_by"].ToString()))
+                        oGiftCards.Created_by = int.Parse(ds.Tables[0].Rows[0]["Created_by"].ToString());
+
+                    oGiftCards.Date = DateTime.Parse(ds.Tables[0].Rows[0]["Date"].ToString());
+                    oGiftCards.Store_id = int.Parse(ds.Tables[0].Rows[0]["Store_id"].ToString());
+                    oGiftCards.Value = decimal.Parse(ds.Tables[0].Rows[0]["Value"].ToString());
+                }
+
+            }
+            return oGiftCards;
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="PoGiftCards"></param>
+        /// <returns></returns>
+        public GiftCard M_Store_Update(GiftCard PoGiftCards)
+        {
+            DataSet ds = new DataSet();
+            GiftCard oGiftCards = new GiftCard();
+            ds = oGiftCardsDAL.M_GiftCards_Update(PoGiftCards.Id,PoGiftCards.Card_no, PoGiftCards.Value, PoGiftCards.Customer_id, PoGiftCards.Balance, PoGiftCards.Expiry
+                , PoGiftCards.Store_id);
+
+            if (ds.Tables.Count > 0)
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    oGiftCards.Id = int.Parse(ds.Tables[0].Rows[0]["Id"].ToString());
+                    oGiftCards.Card_no = ds.Tables[0].Rows[0]["Card_no"].ToString();
+                    oGiftCards.Balance = decimal.Parse(ds.Tables[0].Rows[0]["Balance"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Customer_id"].ToString()))
+                        oGiftCards.Customer_id = int.Parse(ds.Tables[0].Rows[0]["Customer_id"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Expiry"].ToString()))
+                        oGiftCards.Expiry = DateTime.Parse(ds.Tables[0].Rows[0]["Expiry"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Created_by"].ToString()))
+                        oGiftCards.Created_by = int.Parse(ds.Tables[0].Rows[0]["Created_by"].ToString());
+
+                    oGiftCards.Date = DateTime.Parse(ds.Tables[0].Rows[0]["Date"].ToString());
+                    oGiftCards.Store_id = int.Parse(ds.Tables[0].Rows[0]["Store_id"].ToString());
+                    oGiftCards.Value = decimal.Parse(ds.Tables[0].Rows[0]["Value"].ToString());
+                }
+
+            }
+            return oGiftCards;
+        }
+
+        /// <summary>
+        /// M_GiftCards_GetById
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public GiftCard M_GiftCards_GetByCardNo(string CardNo)
+        {
+            DataSet ds = new DataSet();
+            GiftCard oGiftCards = new GiftCard();
+
+            ds = oGiftCardsDAL.M_GiftCards_GetByCardNo(CardNo);
+
+            if (ds.Tables.Count > 0)
+            {
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    oGiftCards.Id = int.Parse(ds.Tables[0].Rows[0]["Id"].ToString());
+                    oGiftCards.Card_no = ds.Tables[0].Rows[0]["Card_no"].ToString();
+                    oGiftCards.Balance = decimal.Parse(ds.Tables[0].Rows[0]["Balance"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Customer_id"].ToString()))
+                        oGiftCards.Customer_id = int.Parse(ds.Tables[0].Rows[0]["Customer_id"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Expiry"].ToString()))
+                        oGiftCards.Expiry = DateTime.Parse(ds.Tables[0].Rows[0]["Expiry"].ToString());
+
+                    if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Created_by"].ToString()))
+                        oGiftCards.Created_by = int.Parse(ds.Tables[0].Rows[0]["Created_by"].ToString());
+
+                    oGiftCards.Date = DateTime.Parse(ds.Tables[0].Rows[0]["Date"].ToString());
+                    oGiftCards.Store_id = int.Parse(ds.Tables[0].Rows[0]["Store_id"].ToString());
+                    oGiftCards.Value = decimal.Parse(ds.Tables[0].Rows[0]["Value"].ToString());
+                }
+
+            }
+            return oGiftCards;
+        }
+    }
+}
---
+++ POS/Controllers/GiftCardsController.cs
@@ -0,0 +1,230 @@
+﻿using DBL;
+using POCO;
+using POS.Models;
+using System;
+using System.Web.Mvc;
+
+namespace POS.Controllers
+{
+    public class GiftCardsController : BaseController
+    {
+        GiftCardDBL oGiftCardDBL;
+        ResultJson result;
+
+        /// <summary>
+        /// GetById
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public ActionResult GetById(int Id)
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oGiftCardDBL = new GiftCardDBL();
+                result.IsSuccess = true;
+                result.Data = oGiftCardDBL.M_GiftCards_GetById(Id);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// GetAll
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetAll()
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oGiftCardDBL = new GiftCardDBL();
+                result.IsSuccess = true;
+                result.Data = oGiftCardDBL.M_GiftCards_GetAll();
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oGiftCardDBL = new GiftCardDBL();
+                oGiftCardDBL.M_GiftCards_Delete(Id);
+                result.IsSuccess = true;
+                return Json(result);
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result);
+            }
+        }
+
+        /// <summary>
+        /// Insert
+        /// </summary>
+        /// <param name="PoGiftCards"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Insert(GiftCard PoGiftCards)
+        {
+            result = new ResultJson();
+            oGiftCardDBL = new GiftCardDBL();
+
+            try
+            {
+                if(string.IsNullOrEmpty(PoGiftCards.Card_no))
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Card No is required");
+                    return Json(result);
+                }
+
+                if (PoGiftCards.Value <= 0)
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Value is required");
+                    return Json(result);
+                }
+
+                var giftCard = oGiftCardDBL.M_GiftCards_GetByCardNo(PoGiftCards.Card_no.Trim());
+
+                if(giftCard.Id > 0) // exist
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Card No is should be unique");
+                    return Json(result);
+                }
+
+                PoGiftCards.Balance = PoGiftCards.Value;
+                result.IsSuccess = true;
+                PoGiftCards.Created_by = SessionManager.GetSessionUserInfo.UserID;
+                result.Data = oGiftCardDBL.M_Store_Insert(PoGiftCards);
+                return Json(result);
+
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                return Json(result);
+            }
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="PoGiftCards"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Update(GiftCard PoGiftCards)
+        {
+            result = new ResultJson();
+            oGiftCardDBL = new GiftCardDBL();
+
+            try
+            {
+                if (string.IsNullOrEmpty(PoGiftCards.Card_no))
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Card No is required");
+                    return Json(result);
+                }
+
+                if (PoGiftCards.Value <= 0)
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Value is required");
+                    return Json(result);
+                }
+
+                var giftCard = oGiftCardDBL.M_GiftCards_GetByCardNo(PoGiftCards.Card_no.Trim());
+
+                if (giftCard.Id > 0 && giftCard.Id != PoGiftCards.Id) // exist
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Card No is should be unique");
+                    return Json(result);
+                }
+
+                var oldGiftCard = oGiftCardDBL.M_GiftCards_GetById(PoGiftCards.Id);
+
+                PoGiftCards.Balance = PoGiftCards.Value - oldGiftCard.Value + oldGiftCard.Balance; // need to be confirmed
+                result.IsSuccess = true;
+                result.Data = oGiftCardDBL.M_Store_Update(PoGiftCards);
+                return Json(result);
+
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult SellGiftCard(GiftCard PoGiftCards)
+        {
+            result = new ResultJson();
+            oGiftCardDBL = new GiftCardDBL();
+
+            try
+            {
+                if (string.IsNullOrEmpty(PoGiftCards.Card_no))
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Card No is required");
+                    return Json(result);
+                }
+
+                if (PoGiftCards.Value <= 0)
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Value is required");
+                    return Json(result);
+                }
+
+                var giftCard = oGiftCardDBL.M_GiftCards_GetByCardNo(PoGiftCards.Card_no.Trim());
+
+                if (giftCard.Id > 0) // exist
+                {
+                    result.IsSuccess = false;
+                    result.Errors.Add("Card No is should be unique");
+                    return Json(result);
+                }
+
+                PoGiftCards.Balance = PoGiftCards.Value;
+                result.IsSuccess = true;
+                PoGiftCards.Created_by = SessionManager.GetSessionUserInfo.UserID;
+                result.Data = oGiftCardDBL.M_Store_Insert(PoGiftCards);
+                return Json(result);
+
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                return Json(result);
+            }
+        }
+    }
+}
