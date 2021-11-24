784687d5f781429309c5ca0e1d6b9f972e02b3ea
Malak Majed <malakalmallak33@gmail.com>
2021-11-24 15:38:53 +0200

    add suppliers dal,dbl,controller

--- DAL/SupplierDAL.cs
+++ DAL/SupplierDAL.cs
@@ -1,12 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DAL
 {
     public class SupplierDAL : Database
     {
+        /// <summary>
+        /// M_Suppliers_GetAll
+        /// </summary>
+        /// <returns></returns>
+        public DataSet M_Suppliers_GetAll()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "M_POS_Suppliers_GetAll";
+
+            return ExDataBase_returnDataSet(cmd);
+        }
+
+        /// <summary>
+        /// M_Suppliers_GetById
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public DataSet M_Suppliers_GetById(int Id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "M_POS_Suppliers_GetById";
+            cmd.Parameters.AddWithValue("@Id", Id);
+            return ExDataBase_returnDataSet(cmd);
+        }
+
+        /// <summary>
+        /// M_Suppliers_Update
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="Name"></param>
+        /// <param name="Cf1"></param>
+        /// <param name="Cf2"></param>
+        /// <param name="Phone"></param>
+        /// <param name="Email"></param>
+        /// <returns></returns>
+        public DataSet M_Suppliers_Update(int Id, string Name, string Cf1, string Cf2, string Phone, string Email)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "M_POS_Suppliers_Update";
+            cmd.Parameters.AddWithValue("@Id", Id);
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@Cf1", Cf1);
+            cmd.Parameters.AddWithValue("@Cf2", Cf2);
+            cmd.Parameters.AddWithValue("@Phone", Phone);
+            cmd.Parameters.AddWithValue("@Email", Email);
+
+            return ExDataBase_returnDataSet(cmd);
+        }
+
+
+        /// <summary>
+        /// M_Suppliers_Insert
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Cf1"></param>
+        /// <param name="Cf2"></param>
+        /// <param name="Phone"></param>
+        /// <param name="Email"></param>
+        /// <returns></returns>
+        public DataSet M_Suppliers_Insert(string Name, string Cf1, string Cf2, string Phone, string Email)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "M_POS_Suppliers_Insert";
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@Cf1", Cf1);
+            cmd.Parameters.AddWithValue("@Cf2", Cf2);
+            cmd.Parameters.AddWithValue("@Phone", Phone);
+            cmd.Parameters.AddWithValue("@Email", Email);
+
+            return ExDataBase_returnDataSet(cmd);
+        }
+
+        /// <summary>
+        /// M_Suppliers_Delete
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public DataSet M_Suppliers_Delete(int Id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "M_POS_Suppliers_Delete";
+            cmd.Parameters.AddWithValue("@Id", Id);
+            return ExDataBase_returnDataSet(cmd);
+        }
+
     }
 }
---
+++ POS/Controllers/SuppliersController.cs
@@ -0,0 +1,133 @@
+﻿using DBL;
+using POCO;
+using POS.Models;
+using System;
+using System.Web.Mvc;
+
+namespace POS.Controllers
+{
+    public class SuppliersController : BaseController
+    {
+        SupplierDBL oSupplierDBL;
+        ResultJson result;
+
+        /// <summary>
+        /// GetById
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public ActionResult GetById(int Id)
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oSupplierDBL = new SupplierDBL();
+                result.IsSuccess = true;
+                result.Data = oSupplierDBL.M_Suppliers_GetById(Id);
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// GetAll
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetAll()
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oSupplierDBL = new SupplierDBL();
+                result.IsSuccess = true;
+                result.Data = oSupplierDBL.M_Suppliers_GetAll();
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        /// <summary>
+        /// Delete
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oSupplierDBL = new SupplierDBL();
+                oSupplierDBL.M_Suppliers_Delete(Id);
+                result.IsSuccess = true;
+                return Json(result);
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result);
+            }
+        }
+
+        /// <summary>
+        /// Insert
+        /// </summary>
+        /// <param name="PoSuppliers"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Insert(Supplier PoSuppliers)
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oSupplierDBL = new SupplierDBL();
+                result.IsSuccess = true;
+                result.Data = oSupplierDBL.M_Store_Insert(PoSuppliers);
+                return Json(result);
+
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                return Json(result);
+            }
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="PoSuppliers"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Update(Supplier PoSuppliers)
+        {
+            result = new ResultJson();
+
+            try
+            {
+                oSupplierDBL = new SupplierDBL();
+                result.IsSuccess = true;
+                result.Data = oSupplierDBL.M_Store_Update(PoSuppliers);
+                return Json(result);
+
+            }
+            catch
+            {
+                result.IsSuccess = false;
+                return Json(result);
+            }
+        }
+    }
+}
